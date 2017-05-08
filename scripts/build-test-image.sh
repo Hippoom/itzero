13282cbdb721083d8db179cb36b718a88b8e868d
hippoom <hippoom@gmail.com>
2017-05-08 15:13:08 +0800

    add build test image to pipeline

--- scripts/build-test-image.sh
+++ scripts/build-test-image.sh
@@ -5,11 +5,4 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 version=$(cat "$project_home"/build/version)
 
-cp "$project_home/src/test/docker/Dockerfile" "$project_home/build/Dockerfile.test"
-cp "$project_home/gradlew"  "$project_home/build/"
-cp "$project_home/build.gradle" "$project_home/build/"
-cp "$project_home/settings.gradle" "$project_home/build/"
-cp -r "$project_home/gradle" "$project_home/build/"
-
-
-docker build -t "$test_image:$version" -f "$project_home"/build/Dockerfile.test "$project_home/build"
+docker build -t "$test_image:$version" -f "$project_home"/src/test/docker/Dockerfile "$project_home"
